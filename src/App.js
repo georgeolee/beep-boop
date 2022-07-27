333d9db137648a32aa3d1f59e37a7f467a58e512
georgeolee <geoliverlee@gmail.com>
2022-07-27 12:16:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [state, setState] = useState(null); //this is a react thing - kind of like an instance variable in a class
+
+  const flip = () => setState(!state);  //toggle in between beeping and booping
+
+  let displayText;  //text to show in div
+  let displayClass; //HTML class attribute
+
+  if(state === false){
+    displayText = 'beep!'
+    displayClass = 'display beep'
+  }
+  else if(state === true){
+    displayText = 'boop!'
+    displayClass = 'display boop'
+  }
+  else{ //first time null    
+    displayText = '...'
+    displayClass = 'display'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className={displayClass}>{displayText}</div>
+      
+      <button onClick={flip}>Press Me!</button>
+
     </div>
   );
 }
